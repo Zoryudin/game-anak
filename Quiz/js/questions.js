d80c1df7bca6d2878f3f8463a2285114cb1e3e22
Zoryudin <samwidiyz@gmail.com>
2023-08-23 21:03:59 +0700

    first commit

---
+++ Quiz/js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Buah apel berwarna apa?....",
+    answer: "Merah",
+    options: [
+      "Merah",
+      "Kuning",
+      "Hijau",
+      "Biru"
+    ]
+  },
+    {
+    numb: 2,
+    question: "(5 + 3 = ....)",
+    answer: "8",
+    options: [
+      "5",
+      "9",
+      "7",
+      "8"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Yang termasuk dalam alat transportasi adalah.....",
+    answer: "Mobil",
+    options: [
+      "Pintu",
+      "Tas",
+      "Mobil",
+      "Meja"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Saudara lebih tua",
+    answer: "Kakak",
+    options: [
+      "Adik",
+      "Kakak",
+      "Sepupu",
+      "Paman"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Mata digunakan untuk",
+    answer: "Melihat",
+    options: [
+      "Mendengar",
+      "Berjalan",
+      "Mencium",
+      "Melihat"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Nama alat untuk menyapu",
+    answer: "Sapu",
+    options: [
+      "Sapu",
+      "Pel",
+      "Penghapus",
+      "Handuk"
+    ]
+  }, 
+    {
+    numb: 7,
+    question: "Jika ingin pandai maka harus rajin...",
+    answer: "Belajar",
+    options: [
+      "Menabung",
+      "Menyontek",
+      "Belajar",
+      "Mencuri"
+    ]
+  },
+    {
+    numb: 8,
+    question: "(11 + 2 )",
+    answer: "13",
+    options: [
+      "14",
+      "12",
+      "13",
+      "20"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Buah yang mengandung vitamin C",
+    answer: "Jeruk",
+    options: [
+      "Wortel",
+      "Jeruk",
+      "Buah Naga",
+      "Durian"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Tempat ibadah agama hindu",
+    answer: "Pura",
+    options: [
+      "Masjid",
+      "klenteng",
+      "Gereja",
+      "Pura"
+    ]
+  },
+];
---
+++ RangkaiKata/js/words.js
@@ -0,0 +1,58 @@
+let words = [
+    {
+        word: "Hewan",
+        hint: "Makhluk hudup"
+    },
+    {
+        word: "Buah",
+        hint: "Makanan sehat"
+    },
+    {
+        word: "Jeruk",
+        hint: "Buah berwarna kuning rasanya asam"
+    },
+    {
+        word: "Kangguru",
+        hint: "Hewan berkantung yang hidup di afrika"
+    },
+    {
+        word: "Kancil",
+        hint: "Hewan cerdas"
+    },
+    {
+        word: "Apel",
+        hint: "Buah berwana merah"
+    },
+    {
+        word: "Pisang",
+        hint: "Buah panjang berwarna kuning"
+    },
+    {
+        word: "Ayam",
+        hint: "Hewan yang berkokok di pagi hari"
+    },
+    {
+        word: "Kelinci",
+        hint: "Hewan yang melompat lompat"
+    },
+    {
+        word: "Semangka",
+        hint: "Buah besar berwarna hijau"
+    },
+    {
+        word: "ular",
+        hint: "Hewan yang berbisa"
+    },
+    {
+        word: "Singa",
+        hint: "raja hutan"
+    },
+    {
+        word: "Jagung",
+        hint: "sayuran yang berwarna kuning"
+    },
+    {
+        word: "Monyet",
+        hint: "Hewan yang bisa memanjat"
+    },
+]
